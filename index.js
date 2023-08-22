4498c32a2a99a4d946c931b92bc66e35af562b37
James Simeon <jamessimeon60@gmail.com>
2023-08-22 11:21:11 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(num1, num2) {
+    return (num1 / num2);
+}
+
+const square = parameter => parameter * parameter;
+
+const add = (parameterOne,parameterTwo) => parameterOne + parameterTwo;
